d0e765fa3a8c39e77e147b5006cf47e1cc6ce679
Alexandre Matos <matosalexandre795@gmail.com>
2025-10-03 16:35:00 -0300

    Entity: Supplier

--- src/main/java/com/MyRecipies/recipies/entities/Supplier.java
+++ src/main/java/com/MyRecipies/recipies/entities/Supplier.java
@@ -1,5 +1,89 @@
 package com.MyRecipies.recipies.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "suppliers")
 public class Supplier {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+
+    @OneToMany(mappedBy = "supplier")
+    private List<Ingredient> ingredients = new ArrayList<>();
+;
+    
+    public Supplier() {
+    }
+
+    public Supplier(Long id, String name, String phone, String email, String address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
 
 }
