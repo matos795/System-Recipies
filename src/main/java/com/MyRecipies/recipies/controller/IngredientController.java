e963978c696de90aca77be295eab0f4e94616cce
Alexandre Matos <matosalexandre795@gmail.com>
2025-10-04 20:10:06 -0300

    Ingredients Controller

--- src/main/java/com/MyRecipies/recipies/controller/IngredientController.java
+++ src/main/java/com/MyRecipies/recipies/controller/IngredientController.java
@@ -1,5 +1,60 @@
 package com.MyRecipies.recipies.controller;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.MyRecipies.recipies.dto.IngredientDTO;
+import com.MyRecipies.recipies.services.IngredientService;
+
+@RestController
+@RequestMapping(value = "ingredients")
 public class IngredientController {
 
+    @Autowired
+    private IngredientService service;
+
+    @GetMapping
+    public ResponseEntity<Page<IngredientDTO>> findAll(Pageable pageable){
+        Page<IngredientDTO> page = service.findAll(pageable);
+        return ResponseEntity.ok(page);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<IngredientDTO> findById(@PathVariable Long id){
+        IngredientDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<IngredientDTO> insert(@RequestBody IngredientDTO dto){
+        dto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<IngredientDTO> update(@PathVariable Long id, @RequestBody IngredientDTO dto){
+        dto = service.update(dto, id);
+        return ResponseEntity.ok(dto);
+}
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
