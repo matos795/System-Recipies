994a0e204d385627e84b7523ff62bca29fef77ec
Alexandre Matos <matosalexandre795@gmail.com>
2025-11-05 18:56:41 -0300

    UserDetails, GrantedAuthority!

--- src/main/java/com/MyRecipies/recipies/entities/Role.java
+++ src/main/java/com/MyRecipies/recipies/entities/Role.java
@@ -3,6 +3,8 @@ package com.MyRecipies.recipies.entities;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.security.core.GrantedAuthority;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -14,7 +16,7 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "tb_role")
-public class Role {
+public class Role implements GrantedAuthority {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -43,6 +45,7 @@ public class Role {
         this.id = id;
     }
 
+    @Override
     public String getAuthority() {
         return authority;
     }
