a86645b9b855fad5c91a55d70c98c3d738a6112f
Alexandre Matos <matosalexandre795@gmail.com>
2025-11-05 19:39:47 -0300

    OAuth2, JWT, password gramt

--- src/main/java/com/MyRecipies/recipies/services/UserService.java
+++ src/main/java/com/MyRecipies/recipies/services/UserService.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.MyRecipies.recipies.entities.Role;
 import com.MyRecipies.recipies.entities.User;
 import com.MyRecipies.recipies.projections.UserDetailsProjection;
 import com.MyRecipies.recipies.repositories.UserRepository;
 
+@Service
 public class UserService implements UserDetailsService{
 
     @Autowired
